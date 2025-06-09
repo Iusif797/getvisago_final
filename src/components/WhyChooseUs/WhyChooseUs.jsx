5519433f924c67460f017f78dfb9e3a617e5ad90
Iusif797 <iusif.mamedov@gmail.com>
2025-06-09 15:28:18 +0200

    Update WhyChooseUs section to match exactly the design

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -6,37 +6,37 @@ import naushniki from '../../assets/naushniki.svg';
 
 const WhyChooseUs = () => {
   return (
-    <section className="why-choose-us-section px-6 py-8 bg-visa-gray-50">
+    <section className="why-choose-us-section">
       <h2>Why Choose Us</h2>
       
-      <div className="flex flex-col gap-4">
-        {/* Fast Processing - фиолетовый блок */}
-        <div className="feature-card purple">
-          <div className="icon-container bg-[#A259F7]">
+      <div className="flex flex-col">
+        {/* Fast Processing - фиолетовый блок с иконкой справа */}
+        <div className="feature-card purple feature-card-right">
+          <span className="feature-text">Fast Processing</span>
+          <div className="feature-icon purple">
             <img src={molnia} alt="Fast Processing" />
           </div>
-          <span className="feature-text">Fast Processing</span>
         </div>
         
-        {/* Simple Application - зеленый блок */}
-        <div className="feature-card green">
-          <div className="icon-container bg-[#06E2A4]">
+        {/* Simple Application - зеленый блок с иконкой слева */}
+        <div className="feature-card green feature-card-left">
+          <div className="feature-icon green">
             <img src={todo} alt="Simple Application" />
           </div>
           <span className="feature-text">Simple Application</span>
         </div>
         
-        {/* Refund Guarantee - фиолетовый блок */}
-        <div className="feature-card purple">
-          <div className="icon-container bg-[#A259F7]">
+        {/* Refund Guarantee - фиолетовый блок с иконкой справа */}
+        <div className="feature-card purple feature-card-right">
+          <span className="feature-text">Refund Guarantee</span>
+          <div className="feature-icon purple">
             <img src={money} alt="Refund Guarantee" />
           </div>
-          <span className="feature-text">Refund Guarantee</span>
         </div>
         
-        {/* Customer Support - зеленый блок */}
-        <div className="feature-card green">
-          <div className="icon-container bg-[#06E2A4]">
+        {/* Customer Support - зеленый блок с иконкой слева */}
+        <div className="feature-card green feature-card-left">
+          <div className="feature-icon green">
             <img src={naushniki} alt="Customer Support" />
           </div>
           <span className="feature-text">Customer Support</span>
